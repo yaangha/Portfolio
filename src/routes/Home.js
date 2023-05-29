ca353143928a58aa8cff6cef079f498f51d0634e
yaangha <y210he@gmail.com>
2023-05-29 23:44:35 +0900

    working...

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -9,11 +9,11 @@ function Home() {
     return (
         <div className={styles.main_box}>
             <div className={styles.content_box}>
-                <div>
-                    <p>안녕하세요, 가나다라마사바 ㅇ아자아아 개발자입니다. 어떤어떠너더너어어 개발자가 되고 싶습니다. 안녕하세요, 가나다라마사바 ㅇ아자아아 개발자입니다. 어떤어떠너더너어어 개발자가 되고 싶습니다. 안녕하세요, 가나다라마사바 ㅇ아자아아 개발자입니다. 어떤어떠너더너어어 개발자가 되고 싶습니다. </p>
-                </div>
-                <img className={styles.picture} src="image/me.jpg" alt="me"/>
                 <div className={styles.title}>HAEUN PORTFOLIO</div>
+                <div>
+                    <div>SHOW</div>
+                    <img className={styles.picture} src="image/me.jpg" alt="me"/>
+                </div>
             </div>
             <AboutMe />
             <Skills />
