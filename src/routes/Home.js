b9f3f77d00bdf08c02ffcefb38d6883f15868d9e
yaangha <y210he@gmail.com>
2023-05-30 14:54:12 +0900

    working...

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,19 +4,27 @@ import styles from "../styles/Home.module.css";
 import Projects from "../components/Projects";
 import Skills from "../components/Skills";
 import GoToTheTop from "../components/GoToTheTop";
-import Sidebar from "../components/Sidebar";
 import { useRef } from "react";
 
 function Home() {
+    const section1 = useRef();
+    const section2 = useRef();
+    const section3 = useRef();
+    const section4 = useRef();
 
-    const element = useRef();
-
+    const handleScroll = (el) => {
+        window.scrollTo({ top: el.current.offsetTop, behavior: "smooth" });
+    };
     return (
         <div className={styles.main_box}>
-
-            <button onClick={() => {element.current.focus()}}>CLICK</button>
-
-            <Sidebar></Sidebar>
+            <div className={styles.navBtn}>
+                <div>
+                    <button onClick={() => handleScroll(section1)}>AboutMe</button>
+                    <button onClick={() => handleScroll(section2)}>Skills</button>
+                    <button onClick={() => handleScroll(section3)}>Projects</button>
+                    <button onClick={() => handleScroll(section4)}>ContactMe</button>
+                </div>
+            </div>
             <div className={styles.content_box}>
                 <div className={styles.title}>HAEUN PORTFOLIO</div>
                 <div>
@@ -24,13 +32,18 @@ function Home() {
                     <img className={styles.picture} src="image/me.jpg" alt="me"/>
                 </div>
             </div>
-            <AboutMe />
-            <Skills />
-            <Projects />
-            <ContactMe />
-
-            <div ref={element}>Here!!!</div>
-
+            <div ref={section1}>
+                <AboutMe />
+            </div>
+            <div ref={section2}>
+                <Skills />
+            </div>
+            <div ref={section3}>
+                <Projects />
+            </div>
+            <div ref={section4}>
+                <ContactMe />
+            </div>
             <GoToTheTop />
         </div>
     );
